f0ac49b4a46695b296dec0ce281558b9a03f8fe1
Robert <g00395612@atu.ie>
2023-09-27 20:30:17 +0100

    First Push

---
+++ src/test/java/ie/atu/lab1_calculator/Lab1CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab1_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
